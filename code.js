5919f11bd1762c3f0eaa6a130c11de718efcb357
zacscodingclub <zbaston@gmail.com>
2016-07-05 12:38:33 -0500

    Done.

--- code.js
+++ code.js
@@ -1,1 +1,18 @@
 'use strict';
+
+function favIceCream(flavor) {
+  var response = "I love " + flavor;
+  return response;
+}
+
+function shouting(phrase) {
+  return phrase.toUpperCase();
+}
+
+function roundDown(num) {
+  return Math.floor(num);
+}
+
+function theTruth() {
+  return true;
+}
